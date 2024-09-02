d2ee755085e367d8f5d61f1165f7aa2e17c9d34f
wabalubdub <noamboam2@gmail.com>
2024-09-02 09:50:43 +0300

    cleaner solution and apparently also faster

--- 1894/chalk/solution.cs
+++ 1894/chalk/solution.cs
@@ -4,16 +4,15 @@ namespace Chalk
 {
 
 public class Solution {
-    public int[] Chalksums; 
+    public long[] Chalksums; 
     public int ChalkReplacer(int[] chalk, int k) {
-        int answerOnFirstRound =createChalkSums(chalk, k);
-        if (answerOnFirstRound != -1){return answerOnFirstRound;}
-        int kAtCriticalRound = k%this.Chalksums[this.Chalksums.Length-1];
+        createChalkSums(chalk);
+        long kAtCriticalRound =  k%this.Chalksums[this.Chalksums.Length-1];
         int indexToSwitchChalk = BinarySearch(this.Chalksums, (i)=>Chalksums[i]<=kAtCriticalRound);
         return indexToSwitchChalk;
     }
 
-        private int BinarySearch(int[] arrayTosearch, Func<int, bool> condition)
+        private int BinarySearch(long[] arrayTosearch, Func<int, bool> condition)
         {
             int left = 0;
             int right = this.Chalksums.Length-1;
@@ -33,20 +32,13 @@ public class Solution {
             return mid;
         }
 
-        private int createChalkSums(int[] chalk, int k)
+        private void createChalkSums(int[] chalk)
         {
-            this.Chalksums = new int[chalk.Length];
+            this.Chalksums = new long[chalk.Length];
             this.Chalksums[0] = chalk[0];
-            if(this.Chalksums[0]>k){
-                    return 0;
-                }
             for (int i = 1;i<chalk.Length;i++){
                 this.Chalksums[i]=this.Chalksums[i-1]+chalk[i];
-                if(this.Chalksums[i]>k){
-                    return i;
-                }
             }
-            return -1;
         }
     }
 }
