3dcc2199d6373f10eeebdb8bd853c2ee0639fb5d
wabalubdub <noamboam2@gmail.com>
2024-09-17 16:00:33 +0300

    improve runtime by not using HashSet for duplication problem 15

--- 15/3Sum/solution.cs
+++ 15/3Sum/solution.cs
@@ -1,35 +1,40 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
-        HashSet<(int x,int y,int z)> returnsets = new HashSet<(int,int,int)>();
         List<IList<int>> returnList = new List<IList<int>>();
         Array.Sort(nums);
         for (int i = 0;i<nums.Length-2;i++){
-            foreach (var set in findPairsNegetevOf(nums[i],i ,nums)){
-                returnsets.Add(set);
+            returnList.AddRange(findPairsNegetevOf(nums[i],i ,nums));
+            while ( i<nums.Length-2 && nums[i]== nums[i+1]){
+                i++;
             }
-        }
-        foreach(var set in returnsets){
-            
-            returnList.Add(new List<int>(){set.x,set.y,set.z});
         }
         return returnList;
         }
 
-    private IEnumerable<(int x,int y,int z)> findPairsNegetevOf(int sumToNegate, int indexToStartFrom, int[] nums)
+    private IEnumerable<List<int>> findPairsNegetevOf(int sumToNegate, int indexToStartFrom, int[] nums)
     {
         int left = indexToStartFrom+1;
         int right = nums.Length-1;
         while (left < right) 
         {
             if (sumToNegate+nums[left]+nums[right]==0){
-                yield return (sumToNegate, nums[left], nums[right]);
+                yield return new List<int>(){sumToNegate, nums[left], nums[right]};
                 left++;
+                while (left < right&&nums[left-1]==nums[left]){
+                    left++;
+                }
             }
             else if (sumToNegate+nums[left]+nums[right]>0){
                 right--;
+                while (left < right && nums[right+1]==nums[right]){
+                    right--;
+                }
             }
             else{
                 left++;
+                while (left < right&&nums[left-1]==nums[left]){
+                    left++;
+                }
             }
 
         }
