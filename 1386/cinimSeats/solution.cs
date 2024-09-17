9bfa159d2def8554f692c887d449013f85a2e74a
wabalubdub <noamboam2@gmail.com>
2024-09-17 18:37:25 +0300

    solved 1386 using bit manipulation

--- 1386/cinimSeats/solution.cs
+++ 1386/cinimSeats/solution.cs
@@ -1,34 +1,37 @@
+
 namespace cinima;
 public class Solution {
     public int MaxNumberOfFamilies(int n, int[][] reservedSeats) {
         int returnValue = 2*n;
-        Dictionary<int,bool[]> takenSeats = new Dictionary<int, bool[]>();
+        Dictionary<int,int> takenSeats = new Dictionary<int, int>();
 
         foreach (int[] seats in reservedSeats){
             int seatnumber = seats[1];
             int row =seats[0];
-            bool doubleOption1 = seatnumber == 2|| seatnumber == 3|| seatnumber==4|| seatnumber ==5;
-            bool doubleOption2 = seatnumber == 6|| seatnumber == 7|| seatnumber==8|| seatnumber ==9;
-            bool singleOption = seatnumber == 4|| seatnumber == 5|| seatnumber==6|| seatnumber ==7;
-            if (doubleOption1||doubleOption2||singleOption) 
+            
+            if (seatnumber!=1 && seatnumber!=10) 
             {
                 if(!takenSeats.ContainsKey(row)){
-                    takenSeats.Add(row,[doubleOption1,doubleOption2,singleOption]);
+                    takenSeats.Add(row,0);
                     returnValue--;
                 }
-                if(takenSeats[row][0]&&takenSeats[row][1]&&takenSeats[row][2]){
+                if(IsFullyTaken(takenSeats[row])){
                     continue;
                 }
 
-                takenSeats[row][0] |= doubleOption1;
-                takenSeats[row][1] |= doubleOption2;
-                takenSeats[row][2] |= singleOption;
-                if(takenSeats[row][0]&&takenSeats[row][1]&&takenSeats[row][2]){
+                takenSeats[row] |= 1<<(seatnumber-1);
+                
+                if(IsFullyTaken(takenSeats[row])){
                 returnValue--;
                 }
             }
         }
         return returnValue;
         
+    }
+
+    private bool IsFullyTaken(int v)
+    {
+        return (v&30) !=0&& (v&120) != 0&& (v&480)!=0;
     }
 }
