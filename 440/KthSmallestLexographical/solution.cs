62bfbfac8f3a3091350b2a5aef361847f56f8c05
wabalubdub <noamboam2@gmail.com>
2024-09-22 16:45:16 +0300

    solve 440 more readable

--- 440/KthSmallestLexographical/solution.cs
+++ 440/KthSmallestLexographical/solution.cs
@@ -1,66 +1,38 @@
 
+
 namespace KthLexograph;
 public class Solution {
     public int FindKthNumber(int n, int k) {
-        int sum =0;
-        int candidate =1;
-        while (sum<k){
-            if (sum + 1 == k){
-                return candidate;
+        int guess = 1;
+        int numbersLeftOfguess = 0;
+
+        while (numbersLeftOfguess<k){
+            if (numbersLeftOfguess+1==k){
+                return guess;
             }
-            int addition =  countNumbersStartingWitPre(candidate,n) ;
-            if (sum + addition >= k)
-            {
-                sum++;
-                candidate*=10;
+            int dif = numbersBetweenThisAndNext(guess,n);
+            if (numbersLeftOfguess +dif<k){
+                guess  = guess+1;
+                numbersLeftOfguess += dif;
             }
             else{
-                candidate++;
-                sum += addition;
+                guess = guess*10;
+                numbersLeftOfguess++;
             }
-        } 
-        return candidate;
+        }
+        return guess;
         
     }
 
-    private bool IsSamePrefix(int candidate, int n)
+    private int numbersBetweenThisAndNext(long guess, long n)
     {
-        while (n>0){
-            if (candidate == n){ return true; }
-            n/=10;
+        long between  = 0;
+        int level = 1;
+        while (guess <= n ){
+            between += Math.Min(level,n-guess+1);
+            level*=10;
+            guess*=10;
         }
-        return false;
+        return (int)between;
     }
-    //if pre doesnt start with the start of n then it will be one of 1,11,111,1111,... ect 
-    public int countNumbersStartingWitPre(long pre,int n) {
-        if (pre > n) 
-            {
-              return 0;
-            }  
-
-        if (IsSamePrefix((int)pre,n)){
-            
-            if (n < pre*10){
-                return 1;
-            }
-            int sum = 0;
-            for (int i=0; i<=9;i++)
-            {
-                sum+=countNumbersStartingWitPre(pre*10+i,n);
-            }
-            sum ++;
-            return sum;
-        }
-        else{
-            int returnVal = 1;
-            while (n > pre*10) 
-            {
-                n=n/10;
-                returnVal=returnVal*10+1;
-            }
-            return returnVal;
-        }
-
-    }
-
 }
