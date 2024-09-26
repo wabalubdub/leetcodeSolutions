0b9abb4f8cebc19234bbeded1c38482d26c53da2
wabalubdub <noamboam2@gmail.com>
2024-09-26 10:41:02 +0300

    21 faster and clearer

--- 21/mergeTwoSorted/solution.cs
+++ 21/mergeTwoSorted/solution.cs
@@ -12,29 +12,13 @@ public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null){ return list2; }
         if (list2 == null){ return list1; }
-        ListNode  head = list1.val<list2.val? list1 :list2;
-        ListNode  current = head;
-        ListNode  other = head==list1? list2: list1;
-        
-        while (current.next != null) 
-        {
-            if (other == null){
-                current = current.next;
-            }
-            else{
-            if (current.next.val < other.val){
-                current = current.next;
-            }
-            else{
-                ListNode temp = current.next;
-                current.next = other;
-                current = other;
-                other = temp;
-            }
-            }
+        if ( list1.val<list2.val){
+            list1.next = MergeTwoLists(list1.next,list2);
+            return list1;
         }
-        current.next = other;
-        return head;
-
+        else {
+            list2.next = MergeTwoLists(list1,list2.next);
+            return list2;
+        }
     }
 }
